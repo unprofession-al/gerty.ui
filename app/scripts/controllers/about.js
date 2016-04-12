618d17ef460fe9d7e478a1cfbc746ff79cb14e4b
Daniel Menet <membership@sontags.ch>
2016-04-12 16:00:52 +0200

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gertyuiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gertyuiApp
+ */
+angular.module('gertyuiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gertyuiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gertyuiApp
+ */
+angular.module('gertyuiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
